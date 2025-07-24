1fbfb8bfce86c4464d6efd17911f8cb464435163
ArtLegends <norik.artemka@gmail.com>
2025-07-24 22:45:15 +0300

    next second comt

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,16 +1,21 @@
-import { Metadata } from "next";
+// import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "MedTravel",
-  description: "MedTravel platform",
-};
+// export const metadata: Metadata = {
+//   title: "MedTravel",
+//   description: "MedTravel platform",
+// };
 
-export default async function HomePage() {
-  return (
-    <main className="flex flex-1 justify-center items-center">
-      <div className="rounded-large bg-content1 px-8 py-12 w-full max-w-screen-xl mx-4 text-center shadow-small">
-        <h1 className="text-3xl font-bold">Home</h1>
-      </div>
-    </main>
-  );
+// export default async function HomePage() {
+//   return (
+//     <main className="flex flex-1 justify-center items-center">
+//       <div className="rounded-large bg-content1 px-8 py-12 w-full max-w-screen-xl mx-4 text-center shadow-small">
+//         <h1 className="text-3xl font-bold">Home</h1>
+//       </div>
+//     </main>
+//   );
+// }
+
+'use client'
+export default function _UserGroupPage() {
+  return null
 }
